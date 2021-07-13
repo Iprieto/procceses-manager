b108c9c9e46d0d1d32463f17ca8d01b9ab7d52cf
Igor Prieto <ipechepare@gmail.com>
2021-07-13 22:13:10 +0200

    Añado uuid al proceso, oculto los campos de creación y actualización y hago fillables el resto

--- app/Models/Process.php
+++ app/Models/Process.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Process extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuids;
+
+    const NOT_STARTED = 'NOT_STARTED';
+    const STARTED = 'STARTED';
+    const FINISHED = 'FINISHED';
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $fillable = [
+        'type',
+        'input',
+        'output',
+        'status',
+        'started_at',
+        'finished_at',
+    ];
 }
