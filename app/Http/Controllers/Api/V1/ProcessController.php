52a03214646580bc177321b92c9e017f3ec5a4ea
Igor Prieto <ipechepare@gmail.com>
2021-07-13 22:11:54 +0200

    Añado rutas y controlador para la api

--- app/Http/Controllers/Api/V1/ProcessController.php
+++ app/Http/Controllers/Api/V1/ProcessController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Contracts\ProcessServiceContract;
 use App\Http\Controllers\Controller;
-use App\Models\Process;
 use Illuminate\Http\Request;
 
 class ProcessController extends Controller
 {
+    private $processServiceContract;
+
+    public function __construct(ProcessServiceContract $processServiceContract)
+    {
+        $this->processService = $processServiceContract;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +22,10 @@ class ProcessController extends Controller
      */
     public function index()
     {
-        //
+        return $this->processService->getAllProcesses();
+        
+        // To get the results paginated use this one:
+        // return $this->processService->getAllProcessesPaginated();
     }
 
     /**
@@ -26,7 +36,7 @@ class ProcessController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $this->processService->storeProcess($request);
     }
 
     /**
@@ -35,9 +45,9 @@ class ProcessController extends Controller
      * @param  \App\Models\Process  $process
      * @return \Illuminate\Http\Response
      */
-    public function show(Process $process)
+    public function show($uuid)
     {
-        //
+        return $this->processService->getProcessById($uuid);
     }
 
     /**
@@ -47,9 +57,9 @@ class ProcessController extends Controller
      * @param  \App\Models\Process  $process
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Process $process)
+    public function update(Request $request, $uuid)
     {
-        //
+        return $this->processService->updateProcess($request, $uuid);
     }
 
     /**
@@ -58,8 +68,30 @@ class ProcessController extends Controller
      * @param  \App\Models\Process  $process
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Process $process)
+    public function destroy($uuid)
     {
-        //
+        return $this->processService->destroyProcess($uuid);
+    }
+
+    /**
+     * Update process status to start
+     *
+     * @param  Uuid  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function start($uuid)
+    {
+        return $this->processService->startProcess($uuid);
+    }
+
+    /**
+     * Update process status to finished
+     *
+     * @param  Uuid  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function finished($uuid)
+    {
+        return $this->processService->finishProcess($uuid);
     }
 }
